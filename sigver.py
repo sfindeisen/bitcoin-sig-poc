b5b9cc7f118ec89f0e6a996dbd01e5b2f4a0b742
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2021-08-23 13:41:57 +0200

    make addr parameter optional

--- sigver.py
+++ sigver.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         add_help=True, allow_abbrev=False, epilog="""This program comes with ABSOLUTELY NO WARRANTY.""")
 
-    parser.add_argument("--addr",    required=True, help="Bitcoin address")
-    parser.add_argument("--message", required=True, help="Message (ASCII)")
-    parser.add_argument("--sig",     required=True, help="signature (base64 encoded)")
+    parser.add_argument("--addr",    required=False, help="Bitcoin address")
+    parser.add_argument("--message", required=True,  help="Message (ASCII)")
+    parser.add_argument("--sig",     required=True,  help="signature (base64 encoded)")
     args = parser.parse_args()

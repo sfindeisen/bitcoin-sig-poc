38a0b7e16a4fb57c86f170e19504ea6c36067442
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2021-08-23 11:45:12 +0200

    initial version

---
+++ sigdecode.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import argparse
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        add_help=True, allow_abbrev=False, epilog="""This program comes with ABSOLUTELY NO WARRANTY.""")
+
+    parser.add_argument("--addr",    required=True, help="Bitcoin address")
+    parser.add_argument("--message", required=True, help="Message (ASCII)")
+    parser.add_argument("--sig",     required=True, help="signature (base64 encoded)")
+    args = parser.parse_args()
